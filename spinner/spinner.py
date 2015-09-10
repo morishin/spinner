d00472059975baa8e9c1fd3081fa2fd5cc5f0f9a
Shintaro Morikawa <sntr92@gmail.com>
2015-09-11 01:20:02 +0900

    Enable to set type and duration

--- spinner/spinner.py
+++ spinner/spinner.py
@@ -8,32 +8,35 @@ import time
 
 
 class SpinnerWriter(object):
-    def __init__(self):
-        self._spinner = self._spinner_gen()
+    def __init__(self, type):
+        self._spinner = self._spinner_gen(type)
 
     @staticmethod
-    def _spinner_gen():
-        while 1:
-            yield '|'
-            yield '/'
-            yield '-'
-            yield '\\'
+    def _spinner_gen(type):
+        if type == 'bar':
+            while 1:
+                yield '|'
+                yield '/'
+                yield '-'
+                yield '\\'
 
     def print_next_frame(self):
-        print(next(self._spinner) + '\033[1D', end='', file=sys.stderr)
+        print('\033[s\033[1K' + next(self._spinner) + '\033[u', end='', file=sys.stderr)
 
 
 class Spin(threading.Thread):
-    def __init__(self):
+    def __init__(self, type='bar', duration=.05):
         super(Spin, self).__init__()
+        self._type = type
+        self._duration = duration
         self._exit = False
 
     def run(self):
-        spinner = SpinnerWriter()
+        spinner = SpinnerWriter(self._type)
         while not self._exit:
             spinner.print_next_frame()
             sys.stderr.flush()
-            time.sleep(.05)
+            time.sleep(self._duration)
 
     def __enter__(self):
         self.start()
